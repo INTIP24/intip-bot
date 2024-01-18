67e3ac4c2b0a8b66d9fa7b4c9db2d03b3b4401e2
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-01-18 13:11:51 +0200

    export jsx components

--- src/lib/jsx/index.ts
+++ src/lib/jsx/index.ts
@@ -13,3 +13,13 @@ export function Fragment<Children extends any[]>({
 }): Children {
   return children;
 }
+
+export * from "./Embed";
+export * from "./formatting";
+export * from "./Modal";
+export * from "./Message";
+export * from "./components/Button";
+export * from "./components/Input";
+export * from "./components/Option";
+export * from "./components/Row";
+export * from "./components/Select";
