d9b48c464ca78e470adca70b7f4912fe1de08327
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-01-28 23:08:02 +0200

    add stuff to createGroup for testing purposes

--- src/commands/createGroup.tsx
+++ src/commands/createGroup.tsx
@@ -1,16 +1,33 @@
 import { ButtonStyle } from "discord.js";
 import { slashCommand } from "../lib/commands";
-import { Button, Row, Message, createElement, Embed } from "../lib/jsx";
+import { Button, Row, Message, createElement, Embed, Field } from "../lib/jsx";
+import { useBoolean, useDescription, useName, useString } from "lib/hooks";
 
 export default slashCommand(function createGroup() {
+  useName("creategroup");
+  useDescription("Create a group");
+  const test = useBoolean("test", "?", { required: true })!;
+  const another = useString("another", "some test stuff", {
+    choices: [
+      { name: "A", value: "aaaa" },
+      { name: "B", value: "bbb" },
+    ],
+  });
+
   return async (interaction) => {
     await interaction.deferReply();
 
     return (
-      <Message>
-        <Embed>Painamalla alla olevaa nappia voit luoda ryhmän.</Embed>
+      <Message ephemeral>
+        <Embed title="Ryhmän luonti">
+          Painamalla alla olevaa nappia voit luoda ryhmän.
+          <Field name="A" value={test ? "Tosi" : "Epätosi"} />
+          <Field name="B" value={another ?? "not_given"} />
+        </Embed>
         <Row>
-          <Button style={ButtonStyle.Success}>Luo Ryhmä</Button>
+          <Button url="https://discord.nrth.xyz" style={ButtonStyle.Success}>
+            Luo Ryhmä
+          </Button>
         </Row>
       </Message>
     );
