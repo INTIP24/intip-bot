12dacd781f9989f06c1efd3f0555cc5c92630620
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-01-28 23:08:28 +0200

    use BotClient in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,40 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import { join } from "node:path";
+import { Events, GatewayIntentBits } from "discord.js";
 import { env } from "./env";
 import { Embed, Field } from "./lib/jsx/Embed";
 import { createElement } from "./lib/jsx";
+import { BotClient } from "./lib/client";
 
-const bot = new Client({
+const bot = new BotClient({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMessages,
   ],
+  commandsPath: join(import.meta.dirname, "commands"),
 });
 
-bot.once(Events.ClientReady, (client) => {
+bot.once(Events.ClientReady, () => {
   console.log("Ready!");
+});
 
-  client.on(Events.MessageDelete, async (message) => {
-    if (message.partial) await message.fetch();
-    console.log(message.content);
+bot.on(Events.MessageDelete, async (message) => {
+  if (message.partial) await message.fetch();
+  console.log(message.content);
 
-    if (message.channel) {
-      message.channel.send({
-        embeds: [
-          <Embed title="test">
-            {message.content}
-            <Field
-              name="test"
-              value="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            />
-          </Embed>,
-        ],
-      });
-    }
-  });
+  if (message.channel) {
+    message.channel.send({
+      embeds: [
+        <Embed title="test">
+          {message.content}
+          <Field
+            name="test"
+            value="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+          />
+        </Embed>,
+      ],
+    });
+  }
 });
 
 bot.on(Events.InteractionCreate, async (interaction) => {
